7652d1080796083ac4857bab12f076a3f373f686
unknown <bensonagala@live.com>
2023-02-08 17:25:53 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtLayout>    
+    <NuxtPage/>
+    </NuxtLayout>   
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
