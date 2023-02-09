cc8b3cf1c7a8b253e00d9bfb436190e0258cac22
unknown <bensonagala@live.com>
2023-02-09 11:15:50 +0300

    debugging

--- pages/index.vue
+++ pages/index.vue
@@ -23,27 +23,35 @@
                 <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
             </div>
             <div class="w-full py-6 z-20">
-               <!--  <form action="POST" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
+
+
+
+                <form accept-charset="UTF-8" v-on:submit.prevent="onSubmit()" method="POST" action="https://script.google.com/macros/s/AKfycbxEyS7uiw4ULhpqjw6tw4_njU4pJR6SuPJhKIInz4rX2KkWiHYEYKr6wQdGjv9ihAiLqA/exec" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
                     <div class="pb-2 pt-3">
-                        <input type="name" name="email" id="email" placeholder="Name" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="text" v-model="email" name="Name" id="email" placeholder="Name" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="pb-2 pt-3">
-                        <input type="name" name="phone_no" id="phone_no" placeholder="Phone Number" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="text" v-model="phone" required="required" name="Phone" id="phone_no" placeholder="Phone Number" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="pb-2 pt-3">
-                        <input type="time" name="time_in" id="time_in" placeholder="Time in" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="time" v-model="time" required="required" name="Time_in" id="time_in" placeholder="Time in" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="pb-2 pt-3">
-                        <input type="name" name="amount_paid" id="amount_paid" placeholder="Amount Paid" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="time" v-model="date" required="required" name="Date" id="date" placeholder="Date" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
+                    </div>
+
+                    <div class="pb-2 pt-3">
+                        <input type="text" v-model="text" required="required" name="Amount_Paid" id="amount_paid" placeholder="Amount Paid" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="pb-2 pt-3">
-                        <input type="name" name="location" id="location" placeholder="Location" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="text" v-model="text" required="required" name="Location" id="location" placeholder="Location" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="pb-2 pt-3">
-                        <input type="name" name="number_plate" id="number_plate" placeholder="Number" class="block w-full p-4 text-lg rounded-sm bg-black">
+                        <input type="text" v-model="text" required="required" name="N_plate" id="number_plate" placeholder="Number" class="form-control block w-full p-4 text-lg rounded-sm bg-black">
                     </div>
                     <div class="px-4 pb-2 pt-3">
-                        <button class="uppercase block w-full p-4 text-lg rounded-full bg-indigo-500 hover:bg-indigo-600 focus:outline-none">submit</button>
+                        <div class="success" v-if="isSuccess">We received your submission, thank you!</div>
+                        <button type="submit" class="uppercase block w-full p-4 text-lg rounded-full bg-indigo-500 hover:bg-indigo-600 focus:outline-none">submit</button>
                     </div>
 
                     <div class="p-4 text-center right-0 left-0 flex justify-center space-x-4 mt-16 lg:hidden ">
@@ -57,14 +65,16 @@
                             <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
                         </a>
                     </div>
-                </form> -->
+                </form>
+
+
+
+<!-- 
 
                 <div>
                     <div>
                       <form
-                        accept-charset="UTF-8"
-                        v-on:submit.prevent="onSubmit()"
-                        method="POST"
+                     xxxxxxxxx
                       >
                         <div>
                           <label>Email address</label>
@@ -100,7 +110,16 @@
                         <button type="submit">Submit</button>
                       </form>
                     </div>
-                  </div>
+                  </div> -->
+
+
+
+
+
+
+
+
+
 
 
             </div>
@@ -123,7 +142,8 @@ export default {
       loading: true,
       name: "",
       email: "",
-      message: "",
+      
+
       isSuccess: false
     };
   },
@@ -135,7 +155,7 @@ export default {
         message: this.message
       };
       axios
-        .post("https://getform.io/f/{unique-endpoint-generated-on-step-1}", data, {
+        .post("", data, {
           headers: {
             Accept: "application/json"
           }
