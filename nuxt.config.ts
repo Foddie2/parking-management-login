ef7a9803fde3b902a09c630f4270418fc9013208
unknown <bensonagala@live.com>
2023-02-13 09:12:37 +0300

    debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-16',
       viewport: 'width=500, initial-scale=1',
-      title: 'My App',
+      title: 'Parking your ride',
       meta: [
         // <meta name="description" content="My amazing site">
         { name: 'description', content: 'My amazing site.' }
