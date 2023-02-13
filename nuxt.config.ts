bb6789938a7fa45bedf9be1d946f152590bfd981
unknown <bensonagala@live.com>
2023-02-13 09:07:13 +0300

    debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Park-my-Ride',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        // { name: 'description', content: 'My amazing site.' }
+      ],
+    }
+  },
   css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
